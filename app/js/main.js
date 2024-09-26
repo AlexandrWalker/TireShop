db8ec619660d4dc11a98978e1fc774adb5a7bf1a
AlexandrWalker <alexandr.d.walker@gmail.com>
2024-09-26 22:56:12 +0700

    add banner block

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,28 @@
 
     var nav = priorityNav.init();
 
+    var swiper = new Swiper(".banner__slider-init", {
+      spaceBetween: 0,
+      centeredSlides: true,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: false,
+      navigation: false,
+      breakpoints: {
+        500: {
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        }
+      }
+    });
   });
 })();
