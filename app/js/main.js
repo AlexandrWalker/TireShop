688a4a9b369d73c7f6d4d6cce1d19698ab6aed06
AlexandrWalker <alexandr.d.walker@gmail.com>
2024-09-28 22:19:26 +0700

    add all block in main page

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,7 @@
 
     var nav = priorityNav.init();
 
-    var swiper = new Swiper(".banner__slider-init", {
+    var banner__slider = new Swiper(".banner__slider-init", {
       spaceBetween: 0,
       centeredSlides: true,
       loop: true,
@@ -11,10 +11,46 @@
         delay: 2500,
         disableOnInteraction: false,
       },
-      pagination: false,
-      navigation: false,
       breakpoints: {
         500: {
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        }
+      }
+    });
+
+    var reviews__slider = new Swiper(".reviews__slider-init", {
+      spaceBetween: 30,
+      slidesPerView: 1,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        500: {
+          slidesPerView: 1,
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        },
+        991: {
+          slidesPerView: 2,
           pagination: {
             el: ".swiper-pagination",
             clickable: true,
--- dist/js/main.js
+++ dist/js/main.js
@@ -3,5 +3,64 @@
 
     var nav = priorityNav.init();
 
+    var banner__slider = new Swiper(".banner__slider-init", {
+      spaceBetween: 0,
+      centeredSlides: true,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        500: {
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        }
+      }
+    });
+
+    var reviews__slider = new Swiper(".reviews__slider-init", {
+      spaceBetween: 30,
+      slidesPerView: 1,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        500: {
+          slidesPerView: 1,
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        },
+        991: {
+          slidesPerView: 2,
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        }
+      }
+    });
   });
 })();
